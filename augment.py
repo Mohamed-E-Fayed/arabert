9be3ff9f5dfcf5e463e6e4bbc64bce88085519b5
Mohamed-E-Fayed <m10fayed@gmail.com>
2021-05-16 09:22:59 +0200

    feat_and_update: download and augment\n I updated download file so that it can download many models on a single run.\n augment.py: works properly and used it in testing many sentences and it works as expected.\n I added also some more examples to augment as well and the code handles both a single input and a list of sentences.

--- augment.py
+++ augment.py
@@ -1,23 +1,33 @@
 from transformers import AutoModel, AutoTokenizer
 from nlpaug.augmenter.word import ContextualWordEmbsAug
 
-from arabert.preprocess import ArabertPreprocessor
+from preprocess import ArabertPreprocessor
 
 
 if __name__=='__main__':
-    model_name="aubmindlab/bert-base-arabertv2"
+    #model_name="aubmindlab/bert-large-arabertv2"
+    model_name="asafaya/bert-large-arabic"
+    #use_segmentation=True
+    use_segmentation=False
     arabert_preprocessor=ArabertPreprocessor(model_name=model_name)
-    tokenizer=AutoTokenizer.from_pretrained(model_name)
-    model=AutoModel.from_pretrained(model_name)
+    #tokenizer=AutoTokenizer.from_pretrained(model_name)
+    #model=AutoModel.from_pretrained(model_name)
     augmenter=ContextualWordEmbsAug(model_path=model_name,
-            action='substitute',
+            action='insert',
             aug_min=1,
-            aug_max=10,
+            aug_max=3,
             device='cpu',
     )
     
-    data="لقد قرأت كتابين مملين"
-    augmented_data=augmenter.augment(data)
+    data=["قرأت كتابين مملين",
+            "هي تحب الفاكهة و الخضراوات",
+            "لم ألعب الكرة يومًا!",
+            "ذهبت إلى الأقصر و أسوان في أجازة",
+    ]
+    prep_data=arabert_preprocessor.preprocess(data) if use_segmentation else data
+    augmented_data=augmenter.augment(prep_data)
     print("data:\n", data)
-    print("augmented data:\n", augmented_data)
+    print("preprocessed data:\n", prep_data)
+    print("augmented data:")
+    print(arabert_preprocessor.unpreprocess(augmented_data)) if use_segmentation else print(augmented_data)
 
--- download_models.py
+++ download_models.py
@@ -1,16 +1,17 @@
 from transformers import AutoTokenizer, AutoModel
-from arabert.preprocess import ArabertPreprocessor
 
 # Mini:   asafaya/bert-mini-arabic
 # Medium: asafaya/bert-medium-arabic
 # Base:   asafaya/bert-base-arabic
 # Large:  asafaya/bert-large-arabic
-model="aubmindlab/bert-base-arabertv2"
-tokenizer = AutoTokenizer.from_pretrained(model)
-model = AutoModel.from_pretrained(model)
+#models=["aubmindlab/bert-base-arabertv2", "aubmindlab/bert-large-arabertv2"]
+models=["aubmindlab/aragpt2-mega"]
+for model_name in models:
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
 
-tokenizer.save_pretrained('bert-base-arabertv2')
-model.save_pretrained('bert-base-arabertv2')
+    tokenizer.save_pretrained(model_name)
+    model.save_pretrained(model_name)
 
 
 #tokenizer_c = AutoTokenizer.from_pretrained("bert-base-chinese")
