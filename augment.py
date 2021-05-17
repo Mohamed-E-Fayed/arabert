000d497d59d749b9364f1a0a7b5dda1974f84e98
Mohamed-E-Fayed <m10fayed@gmail.com>
2021-05-17 13:13:56 +0200

    feat: augment.py can generate sentence from word if architecture is GPT2\n\n I can not use GPT2 in a augmentation method except in contextual word embs for sentence. In it, we give the model the first word, and it generates a sentence based on it.

--- augment.py
+++ augment.py
@@ -1,29 +1,44 @@
+#
+# Note: we should add model.cuda or whatever to run the experiment on GPU instead of CPU
+#
+
 from transformers import AutoModel, AutoTokenizer
 from nlpaug.augmenter.word import ContextualWordEmbsAug
+from nlpaug.augmenter.sentence import ContextualWordEmbsForSentenceAug
 
 from preprocess import ArabertPreprocessor
 
 
 if __name__=='__main__':
-    #model_name="aubmindlab/bert-large-arabertv2"
-    model_name="asafaya/bert-large-arabic"
+    model_name="aubmindlab/aragpt2-mega"
+    #model_name="aubmindlab/bert-large-arabertv02"
+    #model_name="asafaya/bert-large-arabic"
     #use_segmentation=True
     use_segmentation=False
     arabert_preprocessor=ArabertPreprocessor(model_name=model_name)
     #tokenizer=AutoTokenizer.from_pretrained(model_name)
     #model=AutoModel.from_pretrained(model_name)
-    augmenter=ContextualWordEmbsAug(model_path=model_name,
-            action='insert',
-            aug_min=1,
-            aug_max=3,
-            device='cpu',
-    )
+    if 'aragpt2' in model_name.lower():
+        augmenter=ContextualWordEmbsForSentenceAug(model_path=model_name,
+                device='cpu',
+        )
+
+        data=["كتاب", "فاكهة", "سيارة", "كرة القدم", "شائعات", "هذا"]
+    else:
+        augmenter=ContextualWordEmbsAug(model_path=model_name,
+                action='substitute',
+                aug_min=1,
+                aug_max=3,
+                device='cpu',
+        )
     
-    data=["قرأت كتابين مملين",
-            "هي تحب الفاكهة و الخضراوات",
-            "لم ألعب الكرة يومًا!",
-            "ذهبت إلى الأقصر و أسوان في أجازة",
-    ]
+        data=["قرأت كتابين مملين",
+                "هذا الكتاب",
+                "الكتاب هذا",
+                "هي تحب الفاكهة و الخضراوات",
+                "لم ألعب الكرة يومًا!",
+                "ذهبت إلى الأقصر و أسوان في أجازة",
+        ]
     prep_data=arabert_preprocessor.preprocess(data) if use_segmentation else data
     augmented_data=augmenter.augment(prep_data)
     print("data:\n", data)
